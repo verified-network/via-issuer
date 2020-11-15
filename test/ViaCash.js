762fd09a63a2f2d76375196743c592fb2b765d7b
kallolborah <18071236+kallolborah@users.noreply.github.com>
2020-11-15 20:48:50 +0530

    waiting for second callback in cash via-eur test

--- test/ViaCash.js
+++ test/ViaCash.js
@@ -100,8 +100,10 @@ contract("IssuingViaEUR", async (accounts) => {
       console.log("Via-EUR cash token contract ether balance after sending ether:", await web3.eth.getBalance(viaeurCashAddress));
       console.log("Account ether balance after sending ether:", await web3.eth.getBalance(accounts[0]));  
       
-      let callbackToViaOracle = await getFirstEvent(oracle.LogResult({fromBlock:'latest'}));
-      await truffleAssert.createTransactionResult(oracle, callbackToViaOracle.transactionHash);
+      let firstCallbackToViaOracle = await getFirstEvent(oracle.LogResult({fromBlock:'latest'}));
+      await truffleAssert.createTransactionResult(oracle, firstCallbackToViaOracle.transactionHash);
+      let secondCallbackToViaOracle = await getFirstEvent(oracle.LogResult({fromBlock:'latest'}));
+      await truffleAssert.createTransactionResult(oracle, secondCallbackToViaOracle.transactionHash);
 
       console.log("Via oracle ether balance after query:", await web3.eth.getBalance(oracle.address));
       console.log("Account Via-EUR cash token balance after sending ether:", await web3.utils.hexToNumberString(await web3.utils.toHex(await viaeurCash.balanceOf(accounts[0]))));
