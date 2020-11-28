7566dcd8f1df20f7f67fd11d2f81936913e00d1f
kallolborah <18071236+kallolborah@users.noreply.github.com>
2020-11-28 20:28:57 +0530

    setting timeout in test

--- test/ViaCash.js
+++ test/ViaCash.js
@@ -186,7 +186,6 @@ contract("ViaUSDExchange", async (accounts) => {
 */
 contract("ViaUSDRedemption", async (accounts) => {
   it("should send Via-USD to Via-USD cash contract and then get ether sent during issuing process", async (done) => {
-    this.timeout(300000);
     setTimeout(done, 200000);
 
     var abdkMathQuad = await ABDKMathQuad.deployed();
