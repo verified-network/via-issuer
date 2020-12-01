5f88cac19e9dc9c9a7138257b7abdad9c70b6256
bonedaddy <boned4ddy@protonmail.com>
2020-12-01 00:25:54 -0800

    actually fix bond size test

--- test/ViaBond.js
+++ test/ViaBond.js
@@ -14,7 +14,7 @@ web3.setProvider("http://127.0.0.1:8545");
 
 contract("BondContractSize", function(accounts) {
     it("get the size of the Bond contract", function() {
-      return Bond.deployed().then(function(instance) {
+      return BondFactory.deployed().then(function(instance) {
         var bytecode = instance.constructor._json.bytecode;
         var deployed = instance.constructor._json.deployedBytecode;
         var sizeOfB  = bytecode.length / 2;
