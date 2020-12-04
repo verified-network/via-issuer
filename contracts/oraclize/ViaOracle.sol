adbd0e6b063e45f498cdd55b9b8d4ab2cf3ae870
bonedaddy <boned4ddy@protonmail.com>
2020-12-03 23:09:54 -0800

    fix oar setting in oraclize

--- contracts/oraclize/ViaOracle.sol
+++ contracts/oraclize/ViaOracle.sol
@@ -45,7 +45,7 @@ contract ViaOracle is Oracle, usingProvable, Initializable {
         payable
     {
         // note : replace OAR if you are testing Oracle with ethereum-bridge (https://github.com/provable-things/ethereum-bridge)
-        /OAR = OracleAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475); 
+        OAR = OracleAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475); 
         provable_setProof(proofType_TLSNotary | proofStorage_IPFS);
         provable_setCustomGasPrice(4000000000); // i.e. 4 GWei
     }
